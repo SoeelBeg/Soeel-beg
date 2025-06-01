6fd37a506419a0a49029a01204f585e09b82eb60
SoeelBeg <Khansohil50858@gmail.com>
2025-06-01 16:37:51 +0530

    Add Logo.png to repository

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Moon, Sun } from 'lucide-react';
 import { useTheme } from '../../context/ThemeContext';
-import logo from '../../images/logo.png';
+import logo from '../../images/logos/Logo.png';
 
 
 const Navbar: React.FC = () => {
